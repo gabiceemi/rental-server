ad2b0dd35647b73f6412395e5a3dbcfa6f0d77e7
Gabriel Cordova Medeiros <gabiceemi@gmail.com>
2023-05-08 14:33:16 -0300

    seed admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,10 +10,12 @@ import swaggerFile from '../../../swagger.json';
 
 import { router } from './routes';
 
-import "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm";
 
 import "@shared/container";
 import { AppError } from '@shared/errors/AppError';
+
+createConnection();
 
 const app = express();
 
